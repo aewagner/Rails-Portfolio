38d50a91841d623d7d46cbc0ecdd1701fa0f65df
aewagner <austin.e.wagner@gmail.com>
2018-05-17 13:07:26 -0700

    Added nav bar and default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+
+    included do
+        before_action :set_page_default
+    end
+
+   def set_page_default
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @seo_keywords = "Austin Wagner portfolio"
+   end
+    
+end
